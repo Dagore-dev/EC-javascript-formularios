02fa132601dc6689bd020737d24db036cd8c2c0a
Dagore <david.gomez.redondo.dev@gmail.com>
2022-10-18 23:17:15 +0200

    validations

--- ejercicio01/index.js
+++ ejercicio01/index.js
@@ -1,13 +1,23 @@
+/**
+ * @type {HTMLFormElement}
+ */
 const formForm = document.getElementById('form')
 const formFields = {
   name: document.getElementById('name'),
   email: document.getElementById('email'),
   passwd: document.getElementById('passwd'),
   repeatPasswd: document.getElementById('repeat-passwd'),
-  conditions: document.getElementById('conditions')
+  conditions: document.getElementById('conditions'),
+  submit: document.getElementById('submitBtn')
+}
+const regexp = {
+  name: /\w+/,
+  email: /^(.+\@.+\..+)$/,
+  passwd: /^(?=.*\d).{7,}$/
 }
 
 formForm.addEventListener('submit', handleSubmit)
+formForm.addEventListener('change', handleChange)
 
 /**
  * Handles the submission of the formulary and makes the corresponding validations.
@@ -16,5 +26,30 @@ formForm.addEventListener('submit', handleSubmit)
 function handleSubmit(e) {
   e.preventDefault()
 
+  const name = formFields.name.value
+  if (!regexp.name.test(name)) {
+    return formFields.name.focus()
+  }
 
+  const email = formFields.email.value
+  if (!regexp.email.test(email)) {
+    return formFields.email.focus()
+  }
+
+  const passwd = formFields.passwd.value
+  if (!regexp.passwd.test(passwd)) {
+    return formFields.passwd.focus()
+  }
+
+  const repeatPasswd = formFields.repeatPasswd.value
+  if (passwd !== repeatPasswd) {
+    return formFields.repeatPasswd.focus()
+  }
+
+  formForm.submit()
+}
+
+function handleChange() {
+  const conditions = formFields.conditions.checked
+  formFields.submit.disabled = !conditions
 }
